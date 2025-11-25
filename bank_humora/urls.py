664570a0d2cf6d9f27327e066dd2fb1ac9c3104c
Хмура Андрей <khmurahottest@gmail.com>
2025-11-25 17:52:49 +0300

    Add files via upload

---
+++ bank_humora/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('bank_app.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    # Перенаправление для стандартного пути accounts/login/
+    path('accounts/login/', RedirectView.as_view(url='/login/', permanent=True)),
+]
